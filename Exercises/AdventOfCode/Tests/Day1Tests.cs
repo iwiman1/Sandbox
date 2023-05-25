0293f6a2522340b9693227ab43aac159f2efd131
iwiman1 <ignaciowiman@gmail.com>
2023-05-25 12:53:03 -0300

    Finish first part of Day1

--- Exercises/AdventOfCode/Tests/Day1Tests.cs
+++ Exercises/AdventOfCode/Tests/Day1Tests.cs
@@ -1,10 +1,30 @@
+using Day1NS;
+
 namespace Day1Tests;
 
 public class Day1Tests
 {
     [Test]
-    public void Test1()
+    public void CorrectResultTest()
     {
-        Assert.Pass();
+        var expectedResult = 72718;
+        var result = Day1.Day1Script("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/Day1_Input.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void BlankSpacesTest()
+    {
+        var expectedResult = 21000;
+        var result = Day1.Day1Script("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/BlankSpacesTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void InitialAndLastBlankLinesTest()
+    {
+        var expectedResult = 21000;
+        var result = Day1.Day1Script("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/InitialAndLastBlankLinesTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
     }
 }
--- Exercises/AdventOfCode/src/Day1/Day1.cs
+++ Exercises/AdventOfCode/src/Day1/Day1.cs
@@ -1,6 +1,41 @@
-﻿namespace Day1;
+﻿namespace Day1NS;
 
 public class Day1
 {
+    public static int Day1Script(string filePath)
+    {
+        // Initialize needed variables.
+        var maxCalories = 0;
+        var currentTotal = 0;
 
+        // Initialize StreamReader.
+        using StreamReader sr = new StreamReader(filePath);
+
+        string? rawLine;
+
+        // Iterate over the input lines.
+        while ((rawLine = sr.ReadLine()) != null)
+        {
+            // Trim to get rid of unwanted blank spaces.
+            var line = rawLine.Trim();
+
+            // If there's a blank line, the elf has chenged, so we reset the current calorie counter, we skip this iteration and move on with the next elf.
+            if (line == "")
+            {
+                currentTotal = 0;
+                continue;
+            }
+
+            // Add each line to the current total.
+            currentTotal += int.Parse(line);
+            
+            // If the current calories are greater than the max calories, then we update the max calories.
+            if (currentTotal > maxCalories)
+            {
+                maxCalories = currentTotal;
+            }
+        }
+
+        return maxCalories;
+    }
 }
