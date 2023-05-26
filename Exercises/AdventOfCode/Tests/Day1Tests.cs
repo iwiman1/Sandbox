caecac768cf429cdd59f40b06f7a9aa615ee9544
iwiman1 <ignaciowiman@gmail.com>
2023-05-25 22:40:24 -0300

    Finish part2 of Day1. Add tests and refactor to part1

--- Exercises/AdventOfCode/Tests/Day1Tests.cs
+++ Exercises/AdventOfCode/Tests/Day1Tests.cs
@@ -5,26 +5,82 @@ namespace Day1Tests;
 public class Day1Tests
 {
     [Test]
-    public void CorrectResultTest()
+    public void CorrectResultPart1Test()
     {
         var expectedResult = 72718;
-        var result = Day1.Day1Script("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/Day1_Input.txt");
+        var result = Day1.MaxCalories("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/Day1_Input.txt");
         Assert.That(result, Is.EqualTo(expectedResult));
     }
 
     [Test]
-    public void BlankSpacesTest()
+    public void BlankSpacesPart1Test()
     {
         var expectedResult = 21000;
-        var result = Day1.Day1Script("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/BlankSpacesTest.txt");
+        var result = Day1.MaxCalories("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/BlankSpacesTest.txt");
         Assert.That(result, Is.EqualTo(expectedResult));
     }
 
     [Test]
-    public void InitialAndLastBlankLinesTest()
+    public void InitialAndLastBlankLinesPart1Test()
     {
         var expectedResult = 21000;
-        var result = Day1.Day1Script("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/InitialAndLastBlankLinesTest.txt");
+        var result = Day1.MaxCalories("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/InitialAndLastBlankLinesTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void MaxValueIsInTheFirstLinePart1Test()
+    {
+        var expectedResult = 100000;
+        var result = Day1.MaxCalories("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/MaxValueInFirstLineTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void MaxValueIsInTheLastLinePart1Test()
+    {
+        var expectedResult = 100000;
+        var result = Day1.MaxCalories("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/MaxValueInLastLineTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void CorrectResultPart2Test()
+    {
+        var expectedResultPart2 = 213089;
+        var resultPart2 = Day1.SumOfTop3("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/Day1_Input.txt");
+        Assert.That(resultPart2, Is.EqualTo(expectedResultPart2));
+    }
+
+    [Test]
+    public void BlankSpacesPart2Test()
+    {
+        var expectedResult = 41000;
+        var result = Day1.SumOfTop3("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/BlankSpacesTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void InitialAndLastBlankLinesPart2Test()
+    {
+        var expectedResult = 41000;
+        var result = Day1.SumOfTop3("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/InitialAndLastBlankLinesTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void MaxValueIsInTheFirstLinePart2Test()
+    {
+        var expectedResult = 136000;
+        var result = Day1.SumOfTop3("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/MaxValueInFirstLineTest.txt");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void MaxValueIsInTheLastLinePart2Test()
+    {
+        var expectedResult = 136000;
+        var result = Day1.SumOfTop3("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day1/MaxValueInLastLineTest.txt");
         Assert.That(result, Is.EqualTo(expectedResult));
     }
 }
--- Exercises/AdventOfCode/src/Day1/Day1.cs
+++ Exercises/AdventOfCode/src/Day1/Day1.cs
@@ -2,7 +2,7 @@
 
 public class Day1
 {
-    public static int Day1Script(string filePath)
+    public static int MaxCalories(string filePath)
     {
         // Initialize needed variables.
         var maxCalories = 0;
@@ -19,23 +19,104 @@ public class Day1
             // Trim to get rid of unwanted blank spaces.
             var line = rawLine.Trim();
 
-            // If there's a blank line, the elf has chenged, so we reset the current calorie counter, we skip this iteration and move on with the next elf.
+            // If there's a blank line, the current elf has finished.
             if (line == "")
             {
+                // If the current calories are greater than the max calories, then we update the max calories.
+                if (currentTotal > maxCalories)
+                {
+                    maxCalories = currentTotal;
+                }
+                // We reset the current calories and skip to the next iteration.
                 currentTotal = 0;
                 continue;
             }
 
             // Add each line to the current total.
             currentTotal += int.Parse(line);
-            
-            // If the current calories are greater than the max calories, then we update the max calories.
-            if (currentTotal > maxCalories)
-            {
-                maxCalories = currentTotal;
-            }
+        }
+
+        // To catch last elf.
+        if (currentTotal > maxCalories)
+        {
+            maxCalories = currentTotal;
         }
 
         return maxCalories;
+    }
+
+    public static int SumOfTop3(string filePath)
+    {
+        // Initialize needed variables.
+        var maxCalories1 = 0;
+        var maxCalories2 = 0;
+        var maxCalories3 = 0;
+        var currentTotal = 0;
+
+        // Initialize StreamReader.
+        using StreamReader sr = new StreamReader(filePath);
+
+        string? rawLine;
+
+        // Iterate over the input lines.
+        while ((rawLine = sr.ReadLine()) != null)
+        {
+            // Trim to get rid of unwanted blank spaces.
+            var line = rawLine.Trim();
+
+            // If there's a blank line, the current elf has finished.
+            if (line == "")
+            {
+                // We check if this elf is one of the top 3 calorie holder. Then we reset the current calories and skip to the next iteration.
+                if (currentTotal > maxCalories1)
+                {
+                    maxCalories3 = maxCalories2;
+                    maxCalories2 = maxCalories1;
+                    maxCalories1 = currentTotal;
+                    currentTotal = 0;
+                    continue;
+                }
+                if (currentTotal > maxCalories2)
+                {
+                    maxCalories3 = maxCalories2;
+                    maxCalories2 = currentTotal;
+                    currentTotal = 0;
+                    continue;
+                }
+                if (currentTotal > maxCalories3)
+                {
+                    maxCalories3 = currentTotal;
+                    currentTotal = 0;
+                    continue;
+                }
+                currentTotal = 0;
+                continue;
+            }
+
+            // Add each line to the current total.
+            currentTotal += int.Parse(line);
+        }
+
+        // To catch last elf.
+        if (currentTotal > maxCalories1)
+        {
+            maxCalories3 = maxCalories2;
+            maxCalories2 = maxCalories1;
+            maxCalories1 = currentTotal;
+            currentTotal = 0;
+        }
+        if (currentTotal > maxCalories2)
+        {
+            maxCalories3 = maxCalories2;
+            maxCalories2 = currentTotal;
+            currentTotal = 0;
+        }
+        if (currentTotal > maxCalories3)
+        {
+            maxCalories3 = currentTotal;
+            currentTotal = 0;
+        }
+
+        return (maxCalories1 + maxCalories2 + maxCalories3);
     }
 }
