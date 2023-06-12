3af94fad72e23e4a9989f95bf8a9250c7b0a8400
iwiman1 <ignaciowiman@gmail.com>
2023-06-11 21:50:06 -0300

    Finish first iteration of part 2 of Day2 and its tests

--- Exercises/AdventOfCode/Tests/Day2Tests.cs
+++ Exercises/AdventOfCode/Tests/Day2Tests.cs
@@ -5,20 +5,34 @@ namespace AdventOfCodeTests;
 public class Day2Tests
 {
     [Test]
-    public void CorrectResultTest()
+    public void CorrectResultPart1Test()
     {
         var expected = 14375;
-        var actual = Day2.TotalPoints("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day2/Day2_Input.txt");
+        var actual = Day2.TotalPointsPart1("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day2/Day2_Input.txt");
         Assert.That(actual, Is.EqualTo(expected));
     }
 
     [Test]
-    public void BlankSpacesTest()
+    public void BlankSpacesPart1Test()
     {
         var expected = 97;
-        var actual = Day2.TotalPoints("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day2/BlankSpacesTest.txt");
+        var actual = Day2.TotalPointsPart1("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day2/BlankSpacesTest.txt");
         Assert.That(actual, Is.EqualTo(expected));
     }
 
+    [Test]
+    public void CorrectResultPart2Test()
+    {
+        var expected = 10274;
+        var actual = Day2.TotalPointsPart2("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day2/Day2_Input.txt");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 
+    [Test]
+    public void BlankSpacesPart2Test()
+    {
+        var expected = 59;
+        var actual = Day2.TotalPointsPart2("F:/Dev/Ignacio/Exercises/AdventOfCode/Tests/resources/Day2/BlankSpacesTest.txt");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
--- Exercises/AdventOfCode/src/Day1/Day2.cs
+++ Exercises/AdventOfCode/src/Day1/Day2.cs
@@ -45,7 +45,7 @@ public static class Day2
         }
     }
 
-    public static int TotalPoints(string filePath)
+    public static int TotalPointsPart1(string filePath)
     {
         // Initialize StreamReader.
         using StreamReader sr = new(filePath);
@@ -66,6 +66,82 @@ public static class Day2
             var pointsForMyChoice = GetPointsForMyChoice(myChoice);
 
             var pointsForResult = GetPointsForResult(opponentChoice, myChoice);
+
+            var roundPoints = pointsForMyChoice + pointsForResult;
+
+            totalPoints += roundPoints;
+        }
+
+        return totalPoints;
+    }
+
+
+
+
+    private static int GetPointsForResultPart2(string roundResult)
+    {
+        switch (roundResult)
+        {
+            case "X": return 0;
+            case "Y": return 3;
+            case "Z": return 6;
+            default: throw new ArgumentException("Not a valid result");
+        }
+    }
+
+    private static int GetPointsForMyChoicePart2(string opponentChoice, string roundResult)
+    {
+        switch (roundResult)
+        {
+            case "X":
+                switch (opponentChoice)
+                {
+                    case "A": return 3;
+                    case "B": return 1;
+                    case "C": return 2;
+                    default: throw new ArgumentException("Not a valid opponent choice");
+                }
+            case "Y":
+                switch (opponentChoice)
+                {
+                    case "A": return 1;
+                    case "B": return 2;
+                    case "C": return 3;
+                    default: throw new ArgumentException("Not a valid opponent choice");
+                }
+            case "Z":
+                switch (opponentChoice)
+                {
+                    case "A": return 2;
+                    case "B": return 3;
+                    case "C": return 1;
+                    default: throw new ArgumentException("Not a valid opponent choice");
+                }
+            default: throw new ArgumentException("Not a valid result");
+        }
+    }
+
+    public static int TotalPointsPart2(string filePath)
+    {
+        // Initialize StreamReader.
+        using StreamReader sr = new(filePath);
+
+        string? rawLine;
+
+        var totalPoints = 0;
+
+        // Iterate over the input lines.
+        while ((rawLine = sr.ReadLine()) != null)
+        {
+            // Trim to get rid of unwanted blank spaces.
+            var line = rawLine.Trim();
+
+            var opponentChoice = line.Split(' ')[0];
+            var roundResult = line.Split(' ')[1];
+
+            var pointsForMyChoice = GetPointsForMyChoicePart2(opponentChoice, roundResult);
+
+            var pointsForResult = GetPointsForResultPart2(roundResult);
 
             var roundPoints = pointsForMyChoice + pointsForResult;
 
